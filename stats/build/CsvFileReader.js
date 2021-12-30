942c8fe8b8cd9428cb0b8c4c9ca14e7f26f61c22
Latch Jackn <latch.jack@gmail.com>
2021-12-30 12:50:33 +0000

    Stripped map logic out into mapRow function

--- stats/build/CsvFileReader.js
+++ stats/build/CsvFileReader.js
@@ -41,17 +41,18 @@ class CsvFileReader {
             .map((row) => {
             return row.split(',');
         })
-            .map((row) => {
-            return [
-                (0, utils_1.dateStringToDate)(row[0]),
-                row[1],
-                row[2],
-                parseInt(row[3]),
-                parseInt(row[4]),
-                row[5],
-                row[6],
-            ];
-        });
+            .map(this.mapRow);
+    }
+    mapRow(row) {
+        return [
+            (0, utils_1.dateStringToDate)(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5],
+            row[6],
+        ];
     }
 }
 exports.CsvFileReader = CsvFileReader;
